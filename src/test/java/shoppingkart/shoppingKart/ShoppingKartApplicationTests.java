fe9f062b6c8a408fd1b36ef2cb8ee7d0a3153fc0
rakshith <rakshithg2000@gmail.com>
2023-09-16 12:21:52 +0530

    first commit

---
+++ src/test/java/shoppingkart/shoppingKart/ShoppingKartApplicationTests.java
@@ -0,0 +1,13 @@
+package shoppingkart.shoppingKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
